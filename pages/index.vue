672f0fac72f1dc63c3e82fd5318b91d6c6e545b8
Anharu <haruan2394@gmail.com>
2020-05-15 05:16:56 +0900

    initail commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <h1 class="title">
+        プライバシーポリシー
+      </h1>
+      <div>
+        <p>当サイト,モバイルアプリ（以下「本サービス」といいます）における、ユーザーについての個人情報を含む利用者情報の取り扱いについて、以下のとおりプライバシーポリシー（以下「本ポリシー」といいます）を定めます。</p>
+      </div>
+      <h2 class="subtitle">個人情報の取得</h2>
+      <p>本サービスは、適正に個人情報を取得いたします。</p>
+      <h2 class="subtitle">個人情報の管理</h2>
+      <p>本サービスは、お客さまの個人情報を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます。</p>
+      <p>2019年5月15日制定</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
